d9c669a6c196c8dc79414cb4060ee886304e3cbc
AzizbekFayziyev <fayziyev.dev@gmail.com>
2024-08-22 13:41:41 +0500

    updated

--- .output/public/_nuxt/BqpZk5iE.js
+++
@@ -1,1 +0,0 @@
-import{q as e}from"./Bk-OGa0e.js";import{V as r,B as t}from"./BGf5y4XT.js";const p={__name:"index",setup(o){return e().push("services/1"),(c,a)=>(r(),t("div"))}};export{p as default};
